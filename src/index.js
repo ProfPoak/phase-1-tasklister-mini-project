c637a2ad9b327e9046c404525b63477a87772ac5
Porter <pdhakes25@gmail.com>
2025-12-06 17:30:31 -0700

    first attempt WIP

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form")
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault()
+  })
+
+  const taskDescription = document.getElementById("new-task-description")
+
+  function buildToDo(taskDescription) {
+    const newLi = document.createElement("li")
+    newLi.textContent = taskDescription
+    task.append(newLi)
+  }
+
+  buildToDo(taskDescription)
+
 });
